2792885081e1611a56e52ccf81984a9da9faa975
Kafukaaa <452369538@qq.com>
2016-09-12 17:12:55 +0800

    'test2'

--- src/A1000.java
+++ src/A1000.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
  * Created by Kafukaaa on 2016/8/26.
  */
 public class A1000 {
-    public static void main(String[] args) throws Exception{
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        System.out.println(a+b);
-    }
+  public static void main(String[] args) throws Exception{
+    Scanner scanner = new Scanner(System.in);
+    int a = scanner.nextInt();
+    int b = scanner.nextInt();
+    System.out.println(a+b);
+    System.out.println("Enheng~!@");
+  }
 }
