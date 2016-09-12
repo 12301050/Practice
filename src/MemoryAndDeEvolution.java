967679fc1686ab7c509f8968e93089ba012ee029
Kafukaaa <452369538@qq.com>
2016-09-12 23:01:24 +0800

    #370.C

---
+++ src/MemoryAndDeEvolution.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * Created by Kafukaaa on 16/9/12.
+ */
+public class MemoryAndDeEvolution {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int x = scanner.nextInt();
+    int y = scanner.nextInt();
+    int a = y, b = y, c = y;
+    int result = 0 ;
+    while (a < x || b < x || c < x){
+      a = b;
+      b = c;
+      c = a + b -1;
+      result++;
+    }
+    System.out.println(result);
+  }
+}
