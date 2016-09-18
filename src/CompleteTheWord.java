db7a38a0ee3effe8b020863b195d391cf8c4c7db
Kafukaaa <452369538@qq.com>
2016-09-18 22:16:33 +0800

    372.B

--- src/CompleteTheWord.java
+++ src/CompleteTheWord.java
@@ -7,41 +7,45 @@ public class CompleteTheWord {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     String c = scanner.next();
-    int[] flag = new int[26];
-    int[] letter = new int[26];
     char[] s = c.toCharArray();
 
-    int sum = 0;
-    for (int i = 0; i < s.length; i++) {
-      if (s[i] == '?'){
-        if (sum < 26) {
-          flag[sum] = i;
-          sum++;
-        }
-      }else {
-        letter[(int)s[i] - 65] = 1;
-      }
-    }
-
-    int account1 = 0;
-    for (int i = 0; i < 26; i++) {
-      if (letter[i] != 1){
-        account1++;
-      }
-    }
-    if (account1 > sum){
-      System.out.println(-1);
-    }else {
-      int account2 = 0;
-      for (int i = 0; i < 26; i++) {
-        if (letter[i] != 1){
-          if (account2 < account1){
-            flag[account2] = (char)(i+65);
-            account2++;
+    int result = 0;
+    for (int i = 0; i < s.length - 25; i++) {
+      int sum1 = 0,sum2 = 0;
+      int[] flag = new int[26];
+      int[] letter = new int[26];
+      for (int j = 0; j < 26; j++) {
+        if (s[i+j] == '?'){
+          flag[sum1] = i+j;
+          sum1 ++;
+        }else {
+          if (letter[(int)s[i+j] - 65] != 1){
+            letter[(int)s[i+j] - 65] = 1;
+            sum2 ++;
           }
         }
       }
-      //TODO fori
+      if (sum1 + sum2 == 26) {
+        result = 1;
+        int sum3 = 0;
+        for (int j = 0; j < 26; j++) {
+          if (letter[j] != 1 ){
+            s[flag[sum3]] = (char)(j + 65);
+            sum3 ++;
+          }
+        }
+        for (int j = 0; j < s.length; j++) {
+          if (s[j] == '?'){
+            s[j] = 'A';
+          }
+        }
+        i = s.length;
+      }
+    }
+    if (result == 1){
+      System.out.println(String.valueOf(s));
+    }else {
+      System.out.println(-1);
     }
   }
 }
