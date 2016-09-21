231eddca837fe4a90d070091c841d00efa8be962
Kafukaaa <452369538@qq.com>
2016-09-21 17:29:31 +0800

    'RCP.Bv2'

--- src/Cactusophobia.java
+++ src/Cactusophobia.java
@@ -11,16 +11,50 @@ public class Cactusophobia {
     int[] start = new int[m];
     int[] end = new int[m];
     int[] color = new int[m];
-    int[] cycle = new  int[m];
+    int[] sumOfColor = new int[m];
+    int[] degree = new  int[m];
     for (int i = 0; i < m; i++) {
       start[i] = scanner.nextInt();
+      degree[start[i]]++;
       end[i] = scanner.nextInt();
+      degree[end[i]]++;
       color[i] = scanner.nextInt();
+      sumOfColor[color[i]]++;
     }
 
-    //计算圈的个数并记录围成每个圈的边
+    int flag = 0;
     for (int i = 0; i < m; i++) {
-
+      if (degree[i] == 1){
+        for (int j = 0; j < m; j++) {
+          if (start[j] == i || end[j] == i){
+            degree[start[j]]--;
+            degree[end[j]]--;
+          }
+        }
+      }
     }
+
+    int s = -1, e = -1;
+    for (int i = 0; i < m; i++) {
+      if (degree[i] == 2){
+        for (int j = 0; j < m; j++) {
+          if (start[j] == i){
+            if (s == -1){
+              s = end[j];
+            }else if (e == -1){
+              e = end[j];
+            }
+          }else if (end[j] == i){
+            if (s == -1){
+              s = end[j];
+            }else if (e == -1){
+              e = end[j];
+            }
+          }
+
+        }
+      }
+    }
+
   }
 }
